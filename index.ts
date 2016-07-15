e4b82082afa588fb83543ab1d5f6d7e2a8b18292
Joakim Beijar <jbeijar@mios.fi>
2016-07-15 11:13:23 +0300

    Initial commit#

---
+++ index.ts
@@ -0,0 +1,31 @@
+import * as gutil from 'gulp-util'
+import * as through from 'through2'
+import { Analyzer } from '../guidestyle/index'
+
+class Options {
+  syntax: string
+}
+export default function(fileName: string, options: Options) {
+  return through.obj(function(file:gutil.File, encoding: string, callback: (err?: Error, data?: gutil.File) => void): void {
+
+		if (file.isNull()) {
+			callback(null, file);
+			return;
+		}
+
+		if (file.isStream()) {
+			callback(new gutil.PluginError('gulp-guidestyle', 'Streaming not supported'));
+			return;
+		}
+
+    let analyzer = new Analyzer();
+    analyzer.analyze(file.basename, options.syntax)
+      .catch(err => callback(err, null))
+      .then(styleguide => {
+        var styleFile = file.clone({contents: false});
+        styleFile.basename = fileName || "styleguide.json";
+        styleFile.contents = new Buffer(JSON.stringify(styleguide, null, 2));
+        callback(null, styleFile);
+      })
+  });
+};
